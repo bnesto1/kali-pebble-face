aa90f92380e354018790268a24149e691ccba4a5
root <devonkearns@gmail.com>
2013-05-01 19:54:09 -0600

    Design overhaul

--- src/kali.c
+++ src/kali.c
@@ -12,10 +12,26 @@ PBL_APP_INFO(MY_UUID,
 Window window;
 
 BmpContainer background_image;	// Background image container
-TextLayer text_time_layer;	// Time layer
+TextLayer text_hour_layer;	// Hour layer
+TextLayer text_minute_layer;	// Minute layer
+Layer line_layer;		// Layer for the horizontal line
+
+/*
 TextLayer text_date_layer;	// Numerical date layer
 TextLayer text_day_layer;	// Day of the week layer
 TextLayer text_month_layer;	// Month layer
+*/
+
+void line_layer_update_callback(Layer *me, GContext* ctx) {
+  (void)me;
+
+  graphics_context_set_stroke_color(ctx, GColorWhite);	// Line will be white
+
+  // Draw 2 lines so it's actually visible
+  graphics_draw_line(ctx, GPoint(12, 115), GPoint(132, 115));
+  graphics_draw_line(ctx, GPoint(12, 116), GPoint(132, 116));
+}
+
 
 void handle_init(AppContextRef ctx) {
   (void)ctx;
@@ -29,42 +45,57 @@ void handle_init(AppContextRef ctx) {
   bmp_init_container(RESOURCE_ID_IMAGE_BACKGROUND_BLK, &background_image);	// Assign the background image
   layer_add_child(&window.layer, &background_image.layer.layer);	// Draw the background
 
-  /* Make the layer for the day of the week.
-  Positioned at 5, 140 with size of 40 x 28 */
+/*
+  // Make the layer for the day of the week.
   text_layer_init(&text_day_layer, window.layer.frame);
   text_layer_set_text_color(&text_day_layer, GColorWhite);		// Set the font colour to white
   text_layer_set_background_color(&text_day_layer, GColorClear);	// Make the layer's background transparent
   layer_set_frame(&text_day_layer.layer, GRect(5, 140, 40, 28));	// Layer at 5, 140 with size 40 x 28
   text_layer_set_font(&text_day_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));	// Custom font
   layer_add_child(&window.layer, &text_day_layer.layer);
+*/
 
-  /* Make the layer for the month.
-  Positioned at 109-45, 140 with size of 40 x 28 */
+/*
+  // Make the layer for the month.
   text_layer_init(&text_month_layer, window.layer.frame);
   text_layer_set_text_color(&text_month_layer, GColorWhite);		// Set the font colour to white
   text_layer_set_background_color(&text_month_layer, GColorClear);	// Make the layer's background transparent
   layer_set_frame(&text_month_layer.layer, GRect(109-45, 140, 40, 28));	// Layer at 109-45,140 with size 40 x 28
   text_layer_set_font(&text_month_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));	// Custom font
   layer_add_child(&window.layer, &text_month_layer.layer);
+*/
 
-  /* Make the layer for the date.
-  Positioned at 109, 140 with size of 30 x 28 */
+/*
+  // Make the layer for the date.
   text_layer_init(&text_date_layer, window.layer.frame);
   text_layer_set_text_color(&text_date_layer, GColorWhite);		// Set the font colour to white
   text_layer_set_background_color(&text_date_layer, GColorClear);	// Make the layer's background transparent
   layer_set_frame(&text_date_layer.layer, GRect(109, 140, 30, 28));	// Layer at 109, 140 with size of 30 x 28
   text_layer_set_font(&text_date_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_21)));	// Custom font
   layer_add_child(&window.layer, &text_date_layer.layer);
+*/
 
-  /* Make the time layer.
-  Positioned at 5, 120 with size of 139 x 48 */
-  text_layer_init(&text_time_layer, window.layer.frame);
-  text_layer_set_text_color(&text_time_layer, GColorWhite);		// Set the font colour to white
-  text_layer_set_background_color(&text_time_layer, GColorClear);	// Make the layer's background transparent
-  text_layer_set_text_alignment(&text_time_layer, GTextAlignmentCenter);	// Centre within the frame
-  layer_set_frame(&text_time_layer.layer, GRect(5, 100, 139, 48));	// Layer at 5, 120 with size of 139 x 48
-  text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_36)));	// Custom font
-  layer_add_child(&window.layer, &text_time_layer.layer);
+
+  // Make the hour layer.
+  text_layer_init(&text_hour_layer, window.layer.frame);
+  text_layer_set_text_color(&text_hour_layer, GColorWhite);		// Set the font colour to white
+  text_layer_set_background_color(&text_hour_layer, GColorClear);	// Make the layer's background transparent
+  layer_set_frame(&text_hour_layer.layer, GRect(12, 120, 60, 40));	// Layer at 5, 120 with size of 139 x 48
+  text_layer_set_font(&text_hour_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_BORIS_BLACKBLOXX_40)));	// Custom font
+  layer_add_child(&window.layer, &text_hour_layer.layer);
+
+  // Make the minute layer
+  text_layer_init(&text_minute_layer, window.layer.frame);
+  text_layer_set_text_color(&text_minute_layer, GColorWhite);             // Set the font colour to white
+  text_layer_set_background_color(&text_minute_layer, GColorClear);       // Make the layer's background transparent
+  layer_set_frame(&text_minute_layer.layer, GRect(72, 120, 60, 40));      // Layer at 5, 120 with size of 139 x 48
+  text_layer_set_font(&text_minute_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_BORIS_BLACKBLOXX_40)));     // Custom font
+  layer_add_child(&window.layer, &text_minute_layer.layer);
+
+  // Make the line layer
+  layer_init(&line_layer, window.layer.frame);
+  line_layer.update_proc = &line_layer_update_callback;
+  layer_add_child(&window.layer, &line_layer);
 
 }
 
@@ -73,40 +104,40 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
   (void)ctx;
 
   // Need to be static because they're used by the system later.
-  static char time_text[] = "00:00";
+  static char hour_text[] = "00";
+  static char minute_text[] = "00";
   static char date_text[] = "00";
   static char day_text[] = "Xxx";
   static char month_text[] = "Xxx";
-  char *time_format;
-
+  char *hour_format;
+  char *minute_format;
 
   // Get the 3-letter abbreviated month
-  string_format_time(month_text, sizeof(month_text), "%b", t->tick_time);
-  text_layer_set_text(&text_month_layer, month_text);
+//  string_format_time(month_text, sizeof(month_text), "%b", t->tick_time);
+//  text_layer_set_text(&text_month_layer, month_text);
 
   // Get just the number of the date
-  string_format_time(date_text, sizeof(date_text), "%e", t->tick_time);
-  text_layer_set_text(&text_date_layer, date_text);
+//  string_format_time(date_text, sizeof(date_text), "%e", t->tick_time);
+//  text_layer_set_text(&text_date_layer, date_text);
 
   // Get the 3-letter abbreviated day of the week
-  string_format_time(day_text, sizeof(day_text), "%a", t->tick_time);
-  text_layer_set_text(&text_day_layer, day_text);
+//  string_format_time(day_text, sizeof(day_text), "%a", t->tick_time);
+//  text_layer_set_text(&text_day_layer, day_text);
 
-  if (clock_is_24h_style()) {
-    time_format = "%R";
-  } else {
-    time_format = "%I:%M";
-  }
+  hour_format = "%H";
+  minute_format = "%M";
 
-  string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
+  string_format_time(hour_text, sizeof(hour_text), hour_format, t->tick_time);
+  string_format_time(minute_text, sizeof(minute_text), minute_format, t->tick_time);
 
   // Kludge to handle lack of non-padded hour format string
   // for twelve hour clock.
-  if (!clock_is_24h_style() && (time_text[0] == '0')) {
-    memmove(time_text, &time_text[1], sizeof(time_text) - 1);
+  if (!clock_is_24h_style() && (hour_text[0] == '0')) {
+    memmove(hour_text, &hour_text[1], sizeof(hour_text) - 1);
   }
 
-  text_layer_set_text(&text_time_layer, time_text);
+  text_layer_set_text(&text_hour_layer, hour_text);
+  text_layer_set_text(&text_minute_layer, minute_text);
 
 }
 
